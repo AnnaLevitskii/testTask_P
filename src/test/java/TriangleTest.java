8b91351c319f2d8f415ff74d4d67e8d033a72fcc
AnnaLevitskii <99040149+AnnaLevitskii@users.noreply.github.com>
2024-04-08 19:33:08 +0300

    refractoring

--- src/test/java/TriangleTest.java
+++ src/test/java/TriangleTest.java
@@ -2,23 +2,28 @@ import enumPuck.triangle.Triangle;
 import enumPuck.triangle.TriangleType;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
 
 public class TriangleTest {
+    SoftAssert softAssert = new SoftAssert();
     @Test
     public void triangleTest_positive(){
-        Assert.assertEquals(Triangle.getTriangleType(90, 50, 40), TriangleType.RIGHTANGLE);
-        Assert.assertEquals(Triangle.getTriangleType(60, 60, 60), TriangleType.EQUILATERAL);
-        Assert.assertEquals(Triangle.getTriangleType(120, 30, 30), TriangleType.ISOSCELES);
-        Assert.assertEquals(Triangle.getTriangleType(80, 60, 40), TriangleType.SCALENE);
+        softAssert.assertEquals(Triangle.getTriangleType(90, 50, 40), TriangleType.RIGHTANGLE);
+        softAssert.assertEquals(Triangle.getTriangleType(60, 60, 60), TriangleType.EQUILATERAL);
+        softAssert.assertEquals(Triangle.getTriangleType(120, 30, 30), TriangleType.ISOSCELES);
+        softAssert.assertEquals(Triangle.getTriangleType(80, 60, 40), TriangleType.SCALENE);
+        softAssert.assertEquals(Triangle.getTriangleType(180, 60, 40), TriangleType.SCALENE);
+        softAssert.assertAll("triangleTest_positive");
     }
     @Test
     public void triangleTest_negative(){
-        Assert.assertEquals(Triangle.getTriangleType(90, 50, 20), TriangleType.INVALID);
-        Assert.assertEquals(Triangle.getTriangleType(80, 50, 51), TriangleType.INVALID);
-        Assert.assertEquals(Triangle.getTriangleType(80, 50, 49), TriangleType.INVALID);
-        Assert.assertEquals(Triangle.getTriangleType(-1, 60, 60), TriangleType.INVALID);
-        Assert.assertEquals(Triangle.getTriangleType(-1, 100, 81), TriangleType.INVALID);
-        Assert.assertEquals(Triangle.getTriangleType(0, 30, 30), TriangleType.INVALID);
-        Assert.assertEquals(Triangle.getTriangleType(0, 100, 80), TriangleType.INVALID);
+        softAssert.assertEquals(Triangle.getTriangleType(90, 50, 20), TriangleType.INVALID);
+        softAssert.assertEquals(Triangle.getTriangleType(80, 50, 51), TriangleType.INVALID);
+        softAssert.assertEquals(Triangle.getTriangleType(80, 50, 49), TriangleType.INVALID);
+        softAssert.assertEquals(Triangle.getTriangleType(-1, 60, 60), TriangleType.INVALID);
+        softAssert.assertEquals(Triangle.getTriangleType(-1, 100, 81), TriangleType.INVALID);
+        softAssert.assertEquals(Triangle.getTriangleType(0, 30, 30), TriangleType.INVALID);
+        softAssert.assertEquals(Triangle.getTriangleType(0, 100, 80), TriangleType.INVALID);
+        softAssert.assertAll("triangleTest_negative");
     }
 }
